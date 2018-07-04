14a7f3555c9736c89269c59f839a2755829d5c0c
Akshaygpag <akshayguruprasad@gmail.com>
2018-07-04 17:15:05 +0530

    removed

--- studentportal/src/main/java/com/student/portal/util/Variable.java
+++
@@ -1,8 +0,0 @@
-package com.student.portal.util;
-
-public interface Variable {
-	 String DBURL = "jdbc:mysql://localhost:3306/student?useSSL=false";
-	 String PASSWORD = "password";
-	 String USER = "akshay";
-	 String CLASS = "com.mysql.jdbc.Driver";
-}
